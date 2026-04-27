889aa255b537bbbd8c792c966e0e93ec5a508907
successugochukwuchi-ux <successugochukwuchi@gmail.com>
2026-04-27 11:56:30 +0100

    Delete Julesflutter directory

--- Julesflutter/android/app/src/main/kotlin/com/example/julesflutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.julesflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Julesflutter/lib/config/firebase_config.dart
+++
@@ -1,8 +0,0 @@
-class FirebaseConfig {
-  static const String apiKey = "AIzaSyDUG_Wkvbt_HntxQ9zDSo6eTjkUFMH_mRM";
-  static const String authDomain = "pantheon-study.firebaseapp.com";
-  static const String projectId = "pantheon-study";
-  static const String storageBucket = "pantheon-study.firebasestorage.app";
-  static const String messagingSenderId = "956262652054";
-  static const String appId = "1:956262652054:android:0b55eee49ede84ecddcab7";
-}
